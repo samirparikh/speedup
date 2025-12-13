257f3a21a334369bebdc7943623a745b7b4b7874
Samir Parikh <siparikh@gmail.com>
2025-12-13 10:00:48 -0500

    update nixpkgs to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A simple script to speed up audio files by 1.8x using ffmpeg";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
